3174f58ba5a74eaf309f539ab8740c81c69cbe71
zhouchao <275869435@qq.com>
2017-05-18 08:58:45 +0800

    格式化

--- src/models/app.js
+++ src/models/app.js
@@ -1,120 +1,120 @@
-import { logout, getUserisLogin } from '../services/app'
-import { routerRedux } from 'dva/router'
-import { parse } from 'qs'
-import { config } from '../utils'
-const { prefix } = config
+import { logout, getUserisLogin } from '../services/app';
+import { routerRedux } from 'dva/router';
+import { parse } from 'qs';
+import { config } from '../utils';
+const { prefix } = config;
 
 export default {
-  namespace: 'app',
+    namespace: 'app',
 
-  state: {
-    user: {},
-    menuPopoverVisible: false,
-    siderFold: localStorage.getItem(`${prefix}siderFold`) === 'true',
-    darkTheme: localStorage.getItem(`${prefix}darkTheme`) === 'true',
-    isNavbar: document.body.clientWidth < 769,
-    navOpenKeys: JSON.parse(localStorage.getItem(`${prefix}navOpenKeys`)) || [],
-  },
-
-  subscriptions: {
-    setup ({ dispatch }) {
-      dispatch({ type: 'getUserisLogin' })
-      let tid
-      window.onresize = () => {
-        clearTimeout(tid)
-        tid = setTimeout(() => {
-          dispatch({ type: 'changeNavbar' })
-        }, 300)
-      }
+    state: {
+        user: {},
+        menuPopoverVisible: false,
+        siderFold: localStorage.getItem(`${prefix}siderFold`) === 'true',
+        darkTheme: localStorage.getItem(`${prefix}darkTheme`) === 'true',
+        isNavbar: document.body.clientWidth < 769,
+        navOpenKeys: JSON.parse(localStorage.getItem(`${prefix}navOpenKeys`)) || []
     },
-  },
 
-  effects: {
-    *getUserisLogin ({
-      payload,
-    }, { call, put }) {
-      const data = yield call(getUserisLogin, parse(payload))
-      if (data.success && data.data) {
-        yield put({ type: 'querySuccess', payload: data.data })
-        if (location.pathname === '/login') {
-          yield put(routerRedux.push('/dashboard'))
+    subscriptions: {
+        setup({ dispatch }) {
+            dispatch({ type: 'getUserisLogin' });
+            let tid;
+            window.onresize = () => {
+                clearTimeout(tid);
+                tid = setTimeout(() => {
+                    dispatch({ type: 'changeNavbar' });
+                }, 300);
+            };
         }
-      } else {
-        if (location.pathname !== '/login') {
-          let from = location.pathname
-          if (location.pathname === '/dashboard') {
-            from = '/dashboard'
-          }
-          window.location = `${location.origin}/login?from=${from}`
+    },
+
+    effects: {
+        *getUserisLogin({
+            payload
+        }, { call, put }) {
+            const data = yield call(getUserisLogin, parse(payload));
+            if (data.success && data.data) {
+                yield put({ type: 'querySuccess', payload: data.data });
+                if (location.pathname === '/login') {
+                    yield put(routerRedux.push('/dashboard'));
+                }
+            } else {
+                if (location.pathname !== '/login') {
+                    let from = location.pathname;
+                    if (location.pathname === '/dashboard') {
+                        from = '/dashboard';
+                    }
+                    window.location = `${location.origin}/login?from=${from}`;
+                }
+            }
+        },
+
+        *logout({
+            payload
+        }, { call, put }) {
+            const data = yield call(logout, parse(payload));
+            if (data.success) {
+                yield put(routerRedux.push('/login'));
+            } else {
+                throw (data);
+            }
+        },
+
+        *changeNavbar({
+            payload
+        }, { put, select }) {
+            const { app } = yield(select(_ => _));
+            const isNavbar = document.body.clientWidth < 769;
+            if (isNavbar !== app.isNavbar) {
+                yield put({ type: 'handleNavbar', payload: isNavbar });
+            }
         }
-      }
     },
 
-    *logout ({
-      payload,
-    }, { call, put }) {
-      const data = yield call(logout, parse(payload))
-      if (data.success) {
-        yield put(routerRedux.push('/login'))
-      } else {
-        throw (data)
-      }
-    },
+    reducers: {
+        querySuccess(state, { payload: user }) {
+            return {
+                ...state,
+                user
+            };
+        },
 
-    *changeNavbar ({
-      payload,
-    }, { put, select }) {
-      const { app } = yield(select(_ => _))
-      const isNavbar = document.body.clientWidth < 769
-      if (isNavbar !== app.isNavbar) {
-        yield put({ type: 'handleNavbar', payload: isNavbar })
-      }
-    },
-  },
+        switchSider(state) {
+            localStorage.setItem(`${prefix}siderFold`, !state.siderFold);
+            return {
+                ...state,
+                siderFold: !state.siderFold
+            };
+        },
 
-  reducers: {
-    querySuccess (state, { payload: user }) {
-      return {
-        ...state,
-        user,
-      }
-    },
+        switchTheme(state) {
+            localStorage.setItem(`${prefix}darkTheme`, !state.darkTheme);
+            return {
+                ...state,
+                darkTheme: !state.darkTheme
+            };
+        },
 
-    switchSider (state) {
-      localStorage.setItem(`${prefix}siderFold`, !state.siderFold)
-      return {
-        ...state,
-        siderFold: !state.siderFold,
-      }
-    },
+        switchMenuPopver(state) {
+            return {
+                ...state,
+                menuPopoverVisible: !state.menuPopoverVisible
+            };
+        },
 
-    switchTheme (state) {
-      localStorage.setItem(`${prefix}darkTheme`, !state.darkTheme)
-      return {
-        ...state,
-        darkTheme: !state.darkTheme,
-      }
-    },
+        handleNavbar(state, { payload }) {
+            return {
+                ...state,
+                isNavbar: payload
+            };
+        },
 
-    switchMenuPopver (state) {
-      return {
-        ...state,
-        menuPopoverVisible: !state.menuPopoverVisible,
-      }
-    },
-
-    handleNavbar (state, { payload }) {
-      return {
-        ...state,
-        isNavbar: payload,
-      }
-    },
-
-    handleNavOpenKeys (state, { payload: navOpenKeys }) {
-      return {
-        ...state,
-        ...navOpenKeys,
-      }
-    },
-  },
-}
+        handleNavOpenKeys(state, { payload: navOpenKeys }) {
+            return {
+                ...state,
+                ...navOpenKeys
+            };
+        }
+    }
+};
--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'dva'
-import { Row, Col, Button } from 'antd'
-import styles from './index.less'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'dva';
+import { Row, Col, Button } from 'antd';
+import styles from './index.less';
 
-function Dashboard ({ dashboard }) {
-  const ToYYTown = (e) => {
-    console.log(e)
-  }
-  const { Citylist } = dashboard
-  return (
-    <div>
-      <Row gutter={24}>
-        {Citylist && Citylist.map((item, key) =>
-          <Col lg={4} md={4} key={key}>
-            <Button type="primary" className={styles.areaButton} onClick={ToYYTown.bind(this, item.id)} > {item.name}</Button>
-          </Col>)
-        }
-      </Row>
-    </div>
-  )
+function Dashboard({ dashboard }) {
+    const ToYYTown = (e) => {
+        console.log(e);
+    };
+    const { Citylist } = dashboard;
+    return (
+        <div>
+            <Row gutter={24}>
+                {Citylist && Citylist.map((item, key) => <Col lg={4} md={4} key={key}>
+                    <Button
+                      type="primary"
+                      className={styles.areaButton}
+                      onClick={ToYYTown.bind(this, item.id)}
+                    >
+                        {item.name}</Button>
+                </Col>)}
+            </Row>
+        </div>
+    );
 }
 
 Dashboard.propTypes = {
-  dashboard: PropTypes.object,
-}
+    dashboard: PropTypes.object
+};
 
-export default connect(({ dashboard }) => ({ dashboard }))(Dashboard)
+export default connect(({ dashboard }) => ({ dashboard }))(Dashboard);
