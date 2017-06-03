f055f4752880712089963c77c760d709871fbd0c
DESKTOP-A6U3KRS\chaos <275869435@qq.com>
2017-06-03 12:17:23 +0800

    测试source tree

--- src/models/login.js
+++ src/models/login.js
@@ -3,43 +3,41 @@ import { routerRedux } from 'dva/router'
 import { queryURL } from '../utils'
 
 export default {
-  namespace: 'login',
-  state: {
-    loginLoading: false,
-  },
+    namespace: 'login',
+    state: {
+        loginLoading: false
+    },
 
-  effects: {
-    *login ({
-      payload,
-    }, { put, call }) {
-      yield put({ type: 'showLoginLoading' })
-      const data = yield call(login, payload)
-      yield put({ type: 'hideLoginLoading' })
-      if (data.success) {
-        const from = queryURL('from')
-        yield put({ type: 'app/query' })
-        if (from) {
-          yield put(routerRedux.push(from))
-        } else {
-          yield put(routerRedux.push('/dashboard'))
+    effects: {
+        *login({ payload }, { put, call }) {
+            yield put({ type: 'showLoginLoading' })
+            const data = yield call(login, payload)
+            yield put({ type: 'hideLoginLoading' })
+            if (data.success) {
+                const from = queryURL('from')
+                yield put({ type: 'app/query' })
+                if (from) {
+                    yield put(routerRedux.push(from))
+                } else {
+                    yield put(routerRedux.push('/dashboard'))
+                }
+            } else {
+                throw data
+            }
         }
-      } else {
-        throw data
-      }
     },
-  },
-  reducers: {
-    showLoginLoading (state) {
-      return {
-        ...state,
-        loginLoading: true,
-      }
-    },
-    hideLoginLoading (state) {
-      return {
-        ...state,
-        loginLoading: false,
-      }
-    },
-  },
+    reducers: {
+        showLoginLoading(state) {
+            return {
+                ...state,
+                loginLoading: true
+            }
+        },
+        hideLoginLoading(state) {
+            return {
+                ...state,
+                loginLoading: false
+            }
+        }
+    }
 }
